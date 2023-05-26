34d2cb018a2d589e10c6e60e372af3d552e60719
Vinicius Bassalobre <bassalobre.vinicius@gmail.com>
2023-05-26 02:54:25 -0300

    commit

--- .next/server/flight-server-css-manifest.js
+++
@@ -1,6 +0,0 @@
-self.__RSC_CSS_MANIFEST={
-  "__entry_css_mods__": {
-    "/Users/mvbassalobre/Documents/projects/nextjs-ecommerce/app/page": [],
-    "/Users/mvbassalobre/Documents/projects/nextjs-ecommerce/app/test/page": []
-  }
-}
